513c8a38a48c07113184d55d029dde3583864984
Aman Thakur <builtbyaman@gmail.com>
2022-02-22 15:46:14 +0530

    starting point for auth

--- src/index.js
+++ src/index.js
@@ -2,9 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { App, serviceWorker } from './app';
+import { getAnalytics } from 'firebase/analytics';
+import { initializeApp } from 'firebase/app';
 
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCDhrMvxRmnQIhabQpMd23CrX8AZ6cwjN8',
+  authDomain: 'reservation-fe876.firebaseapp.com',
+  projectId: 'reservation-fe876',
+  storageBucket: 'reservation-fe876.appspot.com',
+  messagingSenderId: '557705528312',
+  appId: '1:557705528312:web:17c172dfac69d681878c0d',
+  measurementId: 'G-D89QNQ4GCV',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // This is where the magic happens. React renders our App component
 // inside the div with the id "root"
